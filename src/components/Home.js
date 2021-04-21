e5809165fba072c005fc4d294687af1c4483dc71
Shahidraza Moulvi <shahidraza.moulvi@aimedatsolutions.com>
2021-04-21 15:40:49 +0530

    no message

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ function Home() {
                 <button onClick={()=>dispatch(login())}>Login</button>
                 <br />
                 {
-                    log ? <h3>You are logged in</h3> :<h3>If you want to visit admin page Please login</h3>
+                    log ? <h3>You are logged in you can visit admin page</h3> :<h3>If you want to visit admin page Please login</h3>
                 }
                 <button onClick={()=>dispatch(logout())}>Logout</button>
                 <br />
