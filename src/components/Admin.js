aaeb15b20f0a26e4999d19a3a5f94fede88fa00f
Shahidraza Moulvi <shahidraza.moulvi@aimedatsolutions.com>
2021-04-21 15:47:23 +0530

    em

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import {logout} from '../actions/index'
+import {useDispatch} from 'react-redux'
 function Admin() {
+    const dispatch=useDispatch();  
+
+    useEffect(() => {
+        return () => {
+            dispatch(logout())
+        }
+    })
     return (
+
         <div className="admin">
             <h1>
                 Valuable information only admin can see
